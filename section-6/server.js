9c3b7eec25fc7477831a9085761cd2a1d3b46c3f
Pramod Sharma <pramod.sharma@nimapinfotech.com>
2025-11-11 17:16:36 +0530

    completed section 6

---
+++ section-6/server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+// 3. START SERVER
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
